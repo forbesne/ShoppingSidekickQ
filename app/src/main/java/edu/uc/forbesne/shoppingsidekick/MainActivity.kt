fb3ed94bcd1a64ba47ea0736630d86e35ce339cb
JoshuaGyau <42988162+JoshuaGyau@users.noreply.github.com>
2021-02-11 19:46:30 -0500

    Push UI changes to Josh's Branch

--- app/src/main/java/edu/uc/forbesne/shoppingsidekick/MainActivity.kt
+++ app/src/main/java/edu/uc/forbesne/shoppingsidekick/MainActivity.kt
@@ -2,6 +2,9 @@ package edu.uc.forbesne.shoppingsidekick
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import edu.uc.forbesne.shoppingsidekick.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -15,4 +18,29 @@ class MainActivity : AppCompatActivity() {
                     .commitNow()
         }
     }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val inflater = menuInflater;
+        inflater.inflate(R.menu.shopping_sidekick_menu,menu);
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+
+
+        when (item.itemId) {
+            R.id.item2 -> Toast.makeText(this,"cart Selected", Toast.LENGTH_SHORT).show()
+            R.id.item3 -> Toast.makeText(this,"item3 Selected", Toast.LENGTH_SHORT).show()
+            R.id.item4 -> Toast.makeText(this,"item4 Selected", Toast.LENGTH_SHORT).show()
+            R.id.item5 -> Toast.makeText(this,"item5 Selected", Toast.LENGTH_SHORT).show()
+
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 }
