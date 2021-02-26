02c823343525ddab56997959b1051c373e2dd3be
daileytj2 <54542601+daileytj2@users.noreply.github.com>
2021-02-26 17:38:54 -0500

    Added fetch requests for store APIs

--- app/src/main/java/edu/uc/forbesne/shoppingsidekick/service/ProductService.kt
+++ app/src/main/java/edu/uc/forbesne/shoppingsidekick/service/ProductService.kt
@@ -52,4 +52,66 @@ class ProductService {
 
         return _products
     }
+
+    fun fetchAllProductsFromTwoStore() : MutableLiveData<ArrayList<Product>> {
+        var _products = MutableLiveData<ArrayList<Product>>()
+        val service = RetrofitClientInstance.retrofitInstance?.create(IProductDAO::class.java)
+        val call = service?.getAllProductsFromTwoStore()
+        call?.enqueue(object : Callback<ArrayList<Product>> {
+            /**
+             * Invoked when a network exception occurred talking to the server or when an unexpected
+             * exception occurred creating the request or processing the response.
+             */
+            override fun onFailure(call: Call<ArrayList<Product>>, t: Throwable) {
+            }
+
+            /**
+             * Invoked for a received HTTP response.
+             *
+             *
+             * Note: An HTTP response may still indicate an application-level failure such as a 404 or 500.
+             * Call [Response.isSuccessful] to determine if the response indicates success.
+             */
+            override fun onResponse(
+                    call: Call<ArrayList<Product>>,
+                    response: Response<ArrayList<Product>>
+            ) {
+                _products.value = response.body()
+            }
+
+        })
+
+        return _products
+    }
+
+    fun fetchAllProductsFromThreeStore() : MutableLiveData<ArrayList<Product>> {
+        var _products = MutableLiveData<ArrayList<Product>>()
+        val service = RetrofitClientInstance.retrofitInstance?.create(IProductDAO::class.java)
+        val call = service?.getAllProductsFromThreeStore()
+        call?.enqueue(object : Callback<ArrayList<Product>> {
+            /**
+             * Invoked when a network exception occurred talking to the server or when an unexpected
+             * exception occurred creating the request or processing the response.
+             */
+            override fun onFailure(call: Call<ArrayList<Product>>, t: Throwable) {
+            }
+
+            /**
+             * Invoked for a received HTTP response.
+             *
+             *
+             * Note: An HTTP response may still indicate an application-level failure such as a 404 or 500.
+             * Call [Response.isSuccessful] to determine if the response indicates success.
+             */
+            override fun onResponse(
+                    call: Call<ArrayList<Product>>,
+                    response: Response<ArrayList<Product>>
+            ) {
+                _products.value = response.body()
+            }
+
+        })
+
+        return _products
+    }
 }
