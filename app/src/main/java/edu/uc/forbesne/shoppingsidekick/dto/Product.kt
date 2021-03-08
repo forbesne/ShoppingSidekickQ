0d91f06750b57d495b5d7e11957b01ac46d7f98f
salingmw <58449342+salingmw@users.noreply.github.com>
2021-03-07 20:04:57 -0500

    My Code Review Suggestions

--- app/src/main/java/edu/uc/forbesne/shoppingsidekick/dto/Product.kt
+++ app/src/main/java/edu/uc/forbesne/shoppingsidekick/dto/Product.kt
@@ -3,6 +3,17 @@ package edu.uc.forbesne.shoppingsidekick.dto
 //I used the professor's code from his github - https://github.com/discospiff/MyPlantDiaryQ
 // as the base for this code
 
+/**
+ * DTO contains parameters:
+ * id
+ * description
+ * price
+ * price_type
+ * brand
+ * category
+ * UPC
+ * imageURL
+ */
 
 data class Product (var id: Int, var description : String, var price :Float, var price_type:String = "",
                     var brand : String, var category : String, var UPC : String, var imageURL : String) {
