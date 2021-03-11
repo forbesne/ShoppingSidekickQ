dc98770b1d3fb6bfa680b959668a629b09eda22a
benjamin gomori <benjamingomori@gmail.com>
2021-03-11 13:42:54 -0500

    Added some logic in MainViewModel

--- app/src/main/java/edu/uc/forbesne/shoppingsidekick/ui/main/MainViewModel.kt
+++ app/src/main/java/edu/uc/forbesne/shoppingsidekick/ui/main/MainViewModel.kt
@@ -5,8 +5,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
+import edu.uc.forbesne.shoppingsidekick.dto.Cart
 import edu.uc.forbesne.shoppingsidekick.dto.CartItem
 import edu.uc.forbesne.shoppingsidekick.dto.Product
+import edu.uc.forbesne.shoppingsidekick.dto.SearchItem
 import edu.uc.forbesne.shoppingsidekick.service.ProductService
 
 //I used the professor's code from his github - https://github.com/discospiff/MyPlantDiaryQ
@@ -17,24 +19,76 @@ import edu.uc.forbesne.shoppingsidekick.service.ProductService
  *
  */
 class MainViewModel : ViewModel() {
-
-    var products: MutableLiveData<ArrayList<Product>> = MutableLiveData<ArrayList<Product>>()
     var productService: ProductService = ProductService()
+    var productsFromShop1: MutableLiveData<ArrayList<Product>> = MutableLiveData<ArrayList<Product>>()
+    var productsFromShop2: MutableLiveData<ArrayList<Product>> = MutableLiveData<ArrayList<Product>>()
+    var productsFromShop3: MutableLiveData<ArrayList<Product>> = MutableLiveData<ArrayList<Product>>()
+    var cart: Cart = Cart(ArrayList<CartItem>())
+    var searchItemList: ArrayList<SearchItem> = ArrayList<SearchItem>()
     private lateinit var firestore : FirebaseFirestore
 
     init {
+        fetchSop1Products()
+        fetchSop2Products()
+        fetchSop3Products()
         fetchAllProducts()
         firestore = FirebaseFirestore.getInstance()
         firestore.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
     }
 
-    fun fetchAllProducts() {
-        products = productService.fetchAllProductsFromOneStore()
+    private fun fetchSop1Products() {
+        productsFromShop1 = productService.fetchAllProductsFromOneStore()    }
+
+    private fun fetchSop2Products() {
+        productsFromShop2 = productService.fetchAllProductsFromTwoStore()
+    }
+
+    private fun fetchSop3Products() {
+        productsFromShop3 = productService.fetchAllProductsFromThreeStore()
+    }
+
+    fun fetchAllProducts():MutableLiveData<ArrayList<Product>> {
+        //We have same products in all 3 APIs, difference is prices
+        return productsFromShop1
     }
 
     fun fetchProductsByName(productName: String) {
-        products = productService.fetchProductsByName(productName)
+        productsFromShop1 = productService.fetchProductsByName(productName)
     }
+
+
+    fun findCheapestMarket() {
+
+    }
+
+    fun addToCart(product: Product, amount: Int){
+        if(amount == 0) return
+        var cartItem = CartItem(product.description, product.UPC, amount, product.imageURL)
+        cart.addItem(cartItem)
+    }
+
+    fun removeFromCart(cartItem: CartItem){
+        cart.removeItem(cartItem)
+    }
+
+    fun deleteCart(){
+        //find function..
+        cart = Cart(ArrayList<CartItem>())
+    }
+
+    private fun addSearchItem(upc: String, amount:Int) {
+        searchItemList.add(SearchItem(upc, amount))
+    }
+
+    private fun removeSearchItem(searchItem: SearchItem) {
+        searchItemList.remove(searchItem)
+    }
+
+    private fun deleteSearchItemList() {
+        //find function..
+        searchItemList = ArrayList<SearchItem>()
+    }
+
 
     fun save(cartItem: CartItem) {
         firestore.collection("cartItems")
