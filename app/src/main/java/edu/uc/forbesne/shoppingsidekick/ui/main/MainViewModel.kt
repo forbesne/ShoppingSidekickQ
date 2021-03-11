fa0a2a3721a27001462faa212b9cb860e1b1fd5b
benjamin gomori <benjamingomori@gmail.com>
2021-03-11 17:04:12 -0500

    Working on the main view model, work in progress...

--- app/src/main/java/edu/uc/forbesne/shoppingsidekick/ui/main/MainViewModel.kt
+++ app/src/main/java/edu/uc/forbesne/shoppingsidekick/ui/main/MainViewModel.kt
@@ -2,10 +2,7 @@ package edu.uc.forbesne.shoppingsidekick.ui.main
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import edu.uc.forbesne.shoppingsidekick.dto.Cart
-import edu.uc.forbesne.shoppingsidekick.dto.CartItem
-import edu.uc.forbesne.shoppingsidekick.dto.Product
-import edu.uc.forbesne.shoppingsidekick.dto.SearchItem
+import edu.uc.forbesne.shoppingsidekick.dto.*
 import edu.uc.forbesne.shoppingsidekick.service.ProductService
 
 //I used the professor's code from his github - https://github.com/discospiff/MyPlantDiaryQ
@@ -17,20 +14,37 @@ import edu.uc.forbesne.shoppingsidekick.service.ProductService
  */
 class MainViewModel : ViewModel() {
     var productService: ProductService = ProductService()
+    var products: MutableLiveData<ArrayList<Product>> = MutableLiveData<ArrayList<Product>>()
     var productsFromShop1: MutableLiveData<ArrayList<Product>> = MutableLiveData<ArrayList<Product>>()
     var productsFromShop2: MutableLiveData<ArrayList<Product>> = MutableLiveData<ArrayList<Product>>()
     var productsFromShop3: MutableLiveData<ArrayList<Product>> = MutableLiveData<ArrayList<Product>>()
     var cart: Cart = Cart(ArrayList<CartItem>())
+
     var searchItemList: ArrayList<SearchItem> = ArrayList<SearchItem>()
+
+    // A list of object{shopName, shopsPrice}
+    var shops1ProductPriceList :Shops1ProductPriceList = Shops1ProductPriceList(3)
+    // A map/table containing all key value pairs where key = productUPC, value =object{shopName, shopPrice}
+    var productPricesByShopMap: HashMap<String,Shops1ProductPriceList> = HashMap<String, Shops1ProductPriceList>()
 
     init {
         fetchSop1Products()
         fetchSop2Products()
         fetchSop3Products()
+        assignProducts()
+        populateShops1ProductPriceList()
+        createObservableShopsPricesMap()
+    }
+
+    private fun populateShops1ProductPriceList() {
+        shops1ProductPriceList.list[0].shopName = "Shop1"
+        shops1ProductPriceList.list[1].shopName = "Shop2"
+        shops1ProductPriceList.list[2].shopName = "Shop3"
     }
 
     private fun fetchSop1Products() {
-        productsFromShop1 = productService.fetchAllProductsFromOneStore()    }
+        productsFromShop1 = productService.fetchAllProductsFromOneStore()
+    }
 
     private fun fetchSop2Products() {
         productsFromShop2 = productService.fetchAllProductsFromTwoStore()
@@ -40,18 +54,65 @@ class MainViewModel : ViewModel() {
         productsFromShop3 = productService.fetchAllProductsFromThreeStore()
     }
 
+    private fun assignProducts() {
+        //Same products in all 3 APIs, difference is prices
+        products = productsFromShop1
+    }
+
     fun fetchAllProducts():MutableLiveData<ArrayList<Product>> {
-        //We have same products in all 3 APIs, difference is prices
-        return productsFromShop1
+        return products
+    }
+
+    private fun createObservableShopsPricesMap() {
+
+        productsFromShop1.observeForever {
+            it.forEach {
+                product-> {
+                    var arr0 = productPricesByShopMap.get(product.UPC)
+                    arr0!!.list[0].price = product.price
+                    productPricesByShopMap.put(product.UPC, arr0)
+                }
+            }
+        }
+
+        productsFromShop2.observeForever {
+            it.forEach {
+                product-> {
+                    var arr1 = productPricesByShopMap.get(product.UPC)
+                    arr1!!.list[1].price = product.price
+                    productPricesByShopMap.put(product.UPC, arr1)
+                }
+            }
+        }
+
+        productsFromShop3.observeForever {
+            it.forEach {
+                product-> {
+                    var arr2 = productPricesByShopMap.get(product.UPC)
+                    arr2!!.list[1].price = product.price
+                    productPricesByShopMap.put(product.UPC, arr2)
+                }
+            }
+        }
     }
 
     fun fetchProductsByName(productName: String) {
         productsFromShop1 = productService.fetchProductsByName(productName)
     }
 
-
     fun findCheapestMarket() {
+        var cart1Price = 0f
+        var cart2Price = 0f
+        var cart3Price = 0f
+        var itemAmount = 0
+        var itemUPC = ""
 
+
+        cart.itemList.forEach{
+            itemAmount = it.amount
+            itemUPC = it.UPC
+
+        }
     }
 
     fun addToCart(product: Product, amount: Int){
@@ -81,5 +142,4 @@ class MainViewModel : ViewModel() {
         //find function..
         searchItemList = ArrayList<SearchItem>()
     }
-
 }
