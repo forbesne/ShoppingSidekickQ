26108ae2588127d31da2a45ad480941ce16495a9
benjamin gomori <benjamingomori@gmail.com>
2021-03-11 19:25:37 -0500

    Changed dto Shops1ProductPriceList name to ProductPriceList.

--- app/src/main/java/edu/uc/forbesne/shoppingsidekick/dto/ProductPriceList.kt
+++ app/src/main/java/edu/uc/forbesne/shoppingsidekick/dto/ProductPriceList.kt
@@ -1,5 +1,5 @@
 package edu.uc.forbesne.shoppingsidekick.dto
 
-data class Shops1ProductPriceList (var size :Int ) {
+data class ProductPriceList (var size :Int ) {
     val list:Array<ShopProductPrice> = Array<ShopProductPrice>(size){ShopProductPrice()}
 }
