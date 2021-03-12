58c58866d5ac7a74b0cb9ec9bdd55ae5f2642e3c
benjamin gomori <benjamingomori@gmail.com>
2021-03-12 00:36:07 -0500

    Adjusted MainFragment.kt to fit TJ branch.

--- app/src/main/java/edu/uc/forbesne/shoppingsidekick/ui/main/MainFragment.kt
+++ app/src/main/java/edu/uc/forbesne/shoppingsidekick/ui/main/MainFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import edu.uc.forbesne.shoppingsidekick.R
+import edu.uc.forbesne.shoppingsidekick.dto.CartItem
 
 class MainFragment : Fragment() {
 
@@ -24,7 +25,30 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        // => Commented out Until btn is available
+        /*btnAddProduct.setOnClickListener {
+            addCartItem()
+        }*/
     }
 
+    // This will get called when the user clicks on the pop-up window.
+    private fun addCartItem() {
+        // The cart item logic should mostly be in the view model and cart item is not created by the user.
+        // Instead we need only the quantity and a way to identify a product - for all shops - done by UPC
+        // => Commented out until view has these fields
+        /*var quantity = etnQuantity.text.toString()
+        var upc = upc.text.toString() //perhaps this should be stored in a hidden label
+        viewModel.addCartItem(cartItem)*/
+
+
+
+        /*var cartItem = CartItem().apply{
+            productName = lblProductName.text.toString()
+            productBrand = lblProductBrand.text.toString()
+            measurementUnit = lblUnitValue.text.toString()
+            cartId = 0
+            quantity = 1
+        }*/
+    }
 }
