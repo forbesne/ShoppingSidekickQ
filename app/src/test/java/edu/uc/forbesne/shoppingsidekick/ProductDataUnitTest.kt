e12488fbdddd808406e9d06e4af4137e87ecff41
benjamin gomori <benjamingomori@gmail.com>
2021-03-12 18:38:37 -0500

    Working on mocking the mvm function that calls the firebase instantiation.

--- app/src/test/java/edu/uc/forbesne/shoppingsidekick/ProductDataUnitTest.kt
+++ app/src/test/java/edu/uc/forbesne/shoppingsidekick/ProductDataUnitTest.kt
@@ -24,9 +24,12 @@ class ProductDataUnitTest {
 
     @get:Rule
     var rule: TestRule = InstantTaskExecutorRule()
-    lateinit var mvm: MainViewModel
+    //lateinit var mvm: MainViewModel
 
     var productService = mockk<ProductService>()
+    var mvm = mockk<MainViewModel>()
+    val x: Unit = Unit
+
 
     @Test
     fun confirmApple_outputsApple () {
@@ -49,7 +52,7 @@ class ProductDataUnitTest {
     }
 
     private fun givenAFeedOfMockedProductDataAreAvailable() {
-        mvm = MainViewModel()
+        //mvm = MainViewModel()
         createMockData()
     }
 
@@ -64,6 +67,9 @@ class ProductDataUnitTest {
         allProductsLiveData.postValue(allProducts)
         every { productService.fetchProductsByName(or("Apple", "Banana")) } returns allProductsLiveData
         every { productService.fetchProductsByName(not(or("Apple", "Banana"))) } returns MutableLiveData<ArrayList<Product>>()
+        every { mvm.createFirebaseInstance() } returns x
+        every {(mvm.fetchProductsByName("Apple"))} 
+
         mvm.productService = productService
 
     }
