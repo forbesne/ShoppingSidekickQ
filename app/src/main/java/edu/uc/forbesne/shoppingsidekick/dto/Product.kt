e1e79b86b31dfcc81c3c0f830f17559c61ea088d
benjamin gomori <benjamingomori@gmail.com>
2021-03-13 02:54:46 -0500

    small comment changes.

--- app/src/main/java/edu/uc/forbesne/shoppingsidekick/dto/Product.kt
+++ app/src/main/java/edu/uc/forbesne/shoppingsidekick/dto/Product.kt
@@ -1,7 +1,6 @@
 package edu.uc.forbesne.shoppingsidekick.dto
 // based onp professor's code - https://github.com/discospiff/MyPlantDiaryQ
 
-
 data class Product (var id: Int, var description : String, var price :Float, var price_type:String = "",
                     var brand : String, var category : String, var UPC : String, var imageURL : String) {
     override fun toString(): String {
