aff7d23ce219040a2adad80ae030534432061d88
benjamin gomori <benjamingomori@gmail.com>
2021-03-20 01:23:17 -0400

    Product DTO - Adjusted all parameters (except UPC) to have a default value, requiring only the UPC to be passed in.

--- app/src/main/java/edu/uc/forbesne/shoppingsidekick/dto/Product.kt
+++ app/src/main/java/edu/uc/forbesne/shoppingsidekick/dto/Product.kt
@@ -1,8 +1,9 @@
 package edu.uc.forbesne.shoppingsidekick.dto
 // based onp professor's code - https://github.com/discospiff/MyPlantDiaryQ
 
-data class Product (var id: Int, var description : String, var price :Float, var price_type:String = "",
-                    var brand : String, var category : String, var UPC : String, var imageURL : String) {
+data class Product ( var UPC : String, var price :Float = 0f, var price_type:String = "",
+                     var brand : String = "", var category : String="",
+                      var imageURL : String="", var description : String = "",var id: Int=0) {
     override fun toString(): String {
         return "Description: $description, Price: $price, Category: $category"
     }
