876a9a06e46354aeff066465507ee69a71bb160d
benjamin gomori <benjamingomori@gmail.com>
2021-03-20 02:49:24 -0400

    Changed Cart dto logic from ArrayList to HashMap to improve search efficiency. Each time the user adds an item to the cart, the cart needs to search if this product already is part of the cart or not.

--- app/src/main/java/edu/uc/forbesne/shoppingsidekick/dto/Cart.kt
+++ app/src/main/java/edu/uc/forbesne/shoppingsidekick/dto/Cart.kt
@@ -1,16 +1,38 @@
 package edu.uc.forbesne.shoppingsidekick.dto
 
+//itemList is the initial cart data coming in when created
 data class Cart (var itemList : ArrayList<CartItem>) {
 
-    fun addItem(item:CartItem){
-        itemList.add(item)
+    //map of all items currently in cart - more efficient than using an array list, as searching for upc is needed
+    //key = upc, value = cartItem
+    var itemQuantityMap: HashMap<String,CartItem> = HashMap<String, CartItem>()
+
+    //makes sure that map includes all initial items when created
+    init {
+        itemList.forEach {
+            itemQuantityMap.put(it.UPC, it)
+        }
     }
 
-    fun removeItem(cartItem:CartItem){
-        if(cartItem != null) itemList.remove(cartItem)
+    fun addItem(item:CartItem){
+        var exsitingItemInCart = itemQuantityMap.get(item.UPC)
+        if(exsitingItemInCart ==null){
+            itemQuantityMap.put(item.UPC, item)
+        }else{
+            exsitingItemInCart.add(item.quantity)
+            itemQuantityMap.put(item.UPC, exsitingItemInCart)
+        }
+    }
+
+    fun removeItemFromCart(item:CartItem){
+        var exsitingItemInCart = itemQuantityMap.get(item.UPC)
+        if(exsitingItemInCart != null){
+            exsitingItemInCart.remove()
+            itemQuantityMap.put(item.UPC, exsitingItemInCart)
+        }
     }
 
     fun deleteCart(amount: Int){
-        this.itemList = ArrayList<CartItem>()
+        itemQuantityMap.clear()
     }
 }
