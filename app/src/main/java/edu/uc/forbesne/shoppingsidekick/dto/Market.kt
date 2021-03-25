308e13f7475803454769f83bac9543e832ce1c3b
daileytj2 <54542601+daileytj2@users.noreply.github.com>
2021-03-25 17:26:20 -0400

    Added the Market dto and MarketListAdapter files.

---
+++ app/src/main/java/edu/uc/forbesne/shoppingsidekick/dto/Market.kt
@@ -0,0 +1,8 @@
+package edu.uc.forbesne.shoppingsidekick.dto
+
+data class Market (var name : String, var distance : Float = 0f, val cartPrice : Float = 0f,
+                   var id : String) {
+    override fun toString(): String {
+        return "Name: $name, Price: $cartPrice, Distance: $distance"
+    }
+}
