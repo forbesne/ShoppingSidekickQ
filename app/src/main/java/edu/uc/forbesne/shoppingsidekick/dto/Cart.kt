d012e4cbbe53e1cf3db1e05874b833e9d4ac6a28
benjamin gomori <benjamingomori@gmail.com>
2021-03-28 02:06:22 -0400

    Added some comments and repositioned class functions for readability.

--- app/src/main/java/edu/uc/forbesne/shoppingsidekick/dto/Cart.kt
+++ app/src/main/java/edu/uc/forbesne/shoppingsidekick/dto/Cart.kt
@@ -1,46 +1,30 @@
 package edu.uc.forbesne.shoppingsidekick.dto
 
-//itemList is the initial cart data coming in when created
 data class Cart (var size :Int =0) {
 
-    //map of all items currently in cart - more efficient than using an array list, as searching for upc is needed
-    //key = upc, value = cartItem
+    // HashMap of all items in cart - more efficient than using an array list, as searching by UPC is needed
+    // key = upc, value = cartItem
     var itemQuantityMap: HashMap<String,CartItem> = HashMap<String, CartItem>()
 
-    //Currently not being used
-    fun addItems(itemList : ArrayList<CartItem>) {
-        itemList.forEach {
-            addItem(it)
-       }
-    }
-
-    // gets called from MianViewModel from the getCartFromFirebase()
-    // and is updated automatically using the addSnapshotListener
+    // gets called from MainViewModel from the getCartFromFirebase()
+    // and is called automatically using the addSnapshotListener
     fun addItem(item:CartItem){
         val existingItemInCart = itemQuantityMap.get(item.UPC)
 
+        // Adjust cart size
         if(existingItemInCart == null){
-            //itemQuantityMap.put(item.UPC, item)
             size++
         }
 
+        // Used to adjust the cart item's quantity
         itemQuantityMap.put(item.UPC, item)
     }
 
-    // This is still not implemented in the application
-    // once it will we need to change this to use firebase
-    fun removeItemFromCart(item:CartItem){
-        var existingItemInCart = itemQuantityMap.get(item.UPC)
-        if(existingItemInCart != null){
-            existingItemInCart.remove()
-            itemQuantityMap.put(item.UPC, existingItemInCart)
+    // Currently not being used
+    fun addItems(itemList : ArrayList<CartItem>) {
+        itemList.forEach {
+            addItem(it)
         }
-    }
-
-    // This is still not implemented in the application
-    // once it will we need to change this to use firebase
-    fun deleteCart(amount: Int){
-        itemQuantityMap.clear()
     }
 
     fun doesHaveItem(upc:String):Boolean{
@@ -59,6 +43,16 @@ data class Cart (var size :Int =0) {
         var existingItemInCart = itemQuantityMap.get(upc)
         if (existingItemInCart == null) return 0
         return existingItemInCart.quantity
+    }
+
+    // This is still not implemented in the application
+    // once it will we need to change this to be updated from firebase
+    fun removeItemFromCart(item:CartItem){
+        var existingItemInCart = itemQuantityMap.get(item.UPC)
+        if(existingItemInCart != null){
+            existingItemInCart.remove()
+            itemQuantityMap.put(item.UPC, existingItemInCart)
+        }
     }
 
     // This is still not implemented in the application
