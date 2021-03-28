9ec15c9e9fd8c26578e36c5c459731189ee806d2
benjamin gomori <benjamingomori@gmail.com>
2021-03-28 00:02:37 -0400

    Changed the api names, and included markedt data in the api. So now each api returns a json object like this { market{ name: 'market name', distance: 5.3}, producrs: []} This will be stored in a new dto MarketAPIObject.kt. I included the API to clarify what is coming from the API.

---
+++ app/src/main/java/edu/uc/forbesne/shoppingsidekick/dto/MarketApiObject.kt
@@ -0,0 +1,6 @@
+package edu.uc.forbesne.shoppingsidekick.dto
+
+import androidx.lifecycle.MutableLiveData
+
+class MarketApiObject(var market: Market = Market(), var products: ArrayList<Product> = ArrayList<Product>()) {
+}
