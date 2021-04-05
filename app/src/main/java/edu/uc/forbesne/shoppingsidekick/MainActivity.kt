ed9c1d7daa031ba77dd478d887efad431aa01e42
Storm Hamilton <hamilsu@mail.uc.edu>
2021-04-05 01:54:29 -0400

    Top right navigation menu was redundant with the inclusion of the bottom menu. Created a new menu file (shopping_sidekick_bottom_menu.xml) and moved some of the previous menu items into it to not only reduce redundancy but to better adhere to android standards.

--- app/src/main/java/edu/uc/forbesne/shoppingsidekick/MainActivity.kt
+++ app/src/main/java/edu/uc/forbesne/shoppingsidekick/MainActivity.kt
@@ -2,6 +2,9 @@ package edu.uc.forbesne.shoppingsidekick
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import edu.uc.forbesne.shoppingsidekick.ui.main.*
