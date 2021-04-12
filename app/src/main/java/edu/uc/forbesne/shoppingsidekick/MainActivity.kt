585cc734b5ee90a691f0199c8ed46e800348c2a3
Natz Forbes <natzforbes@gmail.com>
2021-04-12 11:58:30 -0400

    Edited login to use menu

--- app/src/main/java/edu/uc/forbesne/shoppingsidekick/MainActivity.kt
+++ app/src/main/java/edu/uc/forbesne/shoppingsidekick/MainActivity.kt
@@ -1,9 +1,13 @@
 package edu.uc.forbesne.shoppingsidekick
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import edu.uc.forbesne.shoppingsidekick.ui.main.*
 import kotlinx.android.synthetic.main.main_activity.*
 
@@ -16,6 +20,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var cartFragment: CartFragment
     private lateinit var storeFragment: StoreFragment
     private lateinit var viewModel: MainViewModel
+    private val AUTH_REQUEST_CODE = 1701
+    private var user : FirebaseUser? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,7 +62,7 @@ class MainActivity : AppCompatActivity() {
                     replaceFragment(storeFragment)
                 }
                 R.id.profile -> {
-                    replaceFragment(storeFragment)
+                    login()
                 }
             }
             true
@@ -107,6 +113,26 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun login() {
+        var providers = arrayListOf(
+                AuthUI.IdpConfig.EmailBuilder().build()
+        )
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setTheme(R.style.Theme_ShoppingSidekick)
+                        .build(), AUTH_REQUEST_CODE
+        )
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK) {
+            if (requestCode == AUTH_REQUEST_CODE) {
+                user = FirebaseAuth.getInstance().currentUser
+            }
+        }
+    }
 
 }
