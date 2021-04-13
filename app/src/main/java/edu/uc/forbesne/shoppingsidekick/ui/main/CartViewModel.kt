96979964c85e78d137a264d7c059f9ad7653ad84
benjamin gomori <benjamingomori@gmail.com>
2021-04-13 00:03:22 -0400

    CartViewModel.kt - logic to MainViewModel and FirebaseService

--- app/src/main/java/edu/uc/forbesne/shoppingsidekick/ui/main/CartViewModel.kt
+++ app/src/main/java/edu/uc/forbesne/shoppingsidekick/ui/main/CartViewModel.kt
@@ -2,13 +2,14 @@ package edu.uc.forbesne.shoppingsidekick.ui.main
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import edu.uc.forbesne.shoppingsidekick.dto.Cart
 import edu.uc.forbesne.shoppingsidekick.dto.CartItem
 import edu.uc.forbesne.shoppingsidekick.service.FirebaseService
 
 class CartViewModel : MainViewModel() {
-    private var _cartItem = MutableLiveData<List<CartItem>>()
+//    private var _cartItem = MutableLiveData<List<CartItem>>()
 
-    internal fun fetchCartItem() {
+    /*internal fun fetchCartItem() {
 
         var firestore = FirebaseService().getFireBaseInstance()
 
@@ -17,9 +18,9 @@ class CartViewModel : MainViewModel() {
             var innerCartItems = querySnapshot?.toObjects(CartItem::class.java)
             _cartItem.postValue(innerCartItems!!)
         }
-    }
+    }*/
 
-    internal var cartItem : MutableLiveData<List<CartItem>>
+ /*   internal var cartItem : MutableLiveData<List<CartItem>>
         get() { return _cartItem}
-        set(value) {_cartItem = value}
+        set(value) {_cartItem = value}*/
 }
