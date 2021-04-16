070843e273f4b0f2ca7c9e03035e918b5b2ee0b2
benjamin gomori <benjamingomori@gmail.com>
2021-04-16 18:49:05 -0400

    Adding another integration test.

--- app/src/test/java/edu/uc/forbesne/shoppingsidekick/ProductDataIntegrationTest.kt
+++ app/src/test/java/edu/uc/forbesne/shoppingsidekick/ProductDataIntegrationTest.kt
@@ -2,8 +2,13 @@ package edu.uc.forbesne.shoppingsidekick
 // based onp professor's code - https://github.com/discospiff/MyPlantDiaryQ
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import edu.uc.forbesne.shoppingsidekick.dto.Cart
+import edu.uc.forbesne.shoppingsidekick.dto.CartItem
 import edu.uc.forbesne.shoppingsidekick.dto.Product
+import edu.uc.forbesne.shoppingsidekick.service.FirebaseService
 import edu.uc.forbesne.shoppingsidekick.ui.main.MainViewModel
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -20,6 +25,8 @@ class ProductDataIntegrationTest {
     @get:Rule
     var rule: TestRule =  InstantTaskExecutorRule()
     lateinit var mvm: MainViewModel
+    lateinit var cart: Cart
+    var firebaseService = mockk<FirebaseService>()
 
     @Test
     fun confirmChips_outputsChips () {
@@ -29,12 +36,12 @@ class ProductDataIntegrationTest {
 
     @Test
     fun requestAllProductsFromShopOne_returnsAllProductsFromShopOne() {
-        givenAFeedOfProductDataAreAvailable()
+        givenAViewModelIsAvailable()
         whenRequestAllProductsFromShopOne()
         thenResultIsNotEmpty()
     }
 
-    private fun givenAFeedOfProductDataAreAvailable() {
+    private fun givenAViewModelIsAvailable() {
         mvm = MainViewModel()
     }
 
@@ -51,5 +58,44 @@ class ProductDataIntegrationTest {
             //does work
             //assertTrue(it.size == 25)
         }
+    }
+
+    @Test
+    fun requestCartFromFirebase_createsCartOfSize4() {
+        givenAViewModelIsAvailable()
+        whenRequestAllCartItemFromFirestore()
+        thenCartSizeIs4()
+    }
+
+    private fun whenRequestAllCartItemFromFirestore(){
+        createCartMuckData()
+        cart = mvm.firebaseService.getCart()
+    }
+
+    private fun thenCartSizeIs4() {
+        assertTrue(cart.size == 4)
+    }
+
+    fun createCartMuckData(){
+        var cart = Cart()
+        var cartItems = ArrayList<CartItem>()
+        var cartItem1 = CartItem("111111111111",1,"item1Url",
+            "First Product","The Best","1 fl oz",1f)
+        var cartItem2 = CartItem("222222222222",2,"item1Url",
+            "Second Product","The Best","2 fl oz",2f)
+        var cartItem3 = CartItem("333333333333",3,"item1Url",
+            "Third Product","The Best","3 fl oz",3f)
+        var cartItem4 = CartItem("444444444444",4,"item1Url",
+            "Fourth Product","The Best","4 pounds",4f)
+
+        cartItems.add(cartItem1)
+        cartItems.add(cartItem2)
+        cartItems.add(cartItem3)
+        cartItems.add(cartItem4)
+
+        cart.addItems(cartItems)
+
+        every {firebaseService.getCart() } returns cart
+        mvm.firebaseService = firebaseService
     }
 }
